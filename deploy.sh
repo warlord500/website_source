d29f04ad7f0199fe56451cd4224dff868fb3350e
jadon belezos <jjbelezos@gmail.com>
2020-07-02 12:35:48 -0700

    deploy now works and makes sure that cgi bin also works as well. additionally it should make the 404 work too with .htaccess too.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 # dynamic content continues to work when running my website
 rm  -R public_html/*
 mv 404.zip public_html 
-# we are never overwriting files at least this time
-# i am having some issues with the upload process
-unzip -o ".htaccess" public_html/404.zip  -d public_html/ 
+#ok my options on this are as follows
+#a solves the fossil reading the cgi script wrong due to incorrect line endings.
+#o overwrites the htaccess that auto spawns on the server
+unzip -ao public_html/404.zip  -d public_html/ 
 chmod +x /home/jadonbel/public_html/cgi-bin/fossil.cgi
