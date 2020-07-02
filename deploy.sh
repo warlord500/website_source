0263660c6a096b7dc7b2c9358b6407f37f026b28
jadon belezos <jjbelezos@gmail.com>
2020-07-02 12:17:11 -0700

    updating my automation for deploying the website

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 # this is a tiny script to be ran after every upload to make sure that
 # dynamic content continues to work when running my website
+rm  -R public_html/*
+mv 404.zip public_html 
+# we are never overwriting files at least this time
+# i am having some issues with the upload process
+unzip -o ".htaccess" public_html/404.zip  -d public_html/ 
 chmod +x /home/jadonbel/public_html/cgi-bin/fossil.cgi
