a15e8fc5b8f69a0bdd3d89fd5bafaf77c41b1d48
jadon belezos <jjbelezos@gmail.com>
2020-07-15 14:21:16 -0700

    updating publish script a little bit so that I spend less time trying to publish makes commiting slightly faster

--- publishing/deploy.sh
+++ publishing/deploy.sh
@@ -2,10 +2,9 @@
 # dynamic content continues to work when running my website
 cd ~
 rm  -R public_html/*
-mv ~/404.tgz public_html 
 #ok my options on this are as follows
 #a solves the fossil reading the cgi script wrong due to incorrect line endings.
 #o overwrites the htaccess that auto spawns on the server
-tar -zxvf  public_html/404.tgz
+tar -zxvf  404.tgz
 sed 
 chmod +x /home/jadonbel/public_html/cgi-bin/fossil.cgi
--- publishing/publish.sh
+++ publishing/publish.sh
@@ -1,6 +1,8 @@
-hugo --cleanDestinationDir
+git add .
+git commit 
 #makes getting the password easier
 /d/portable\ applications/windows/keepass/keepass_classic/KeePass.exe &
+hugo --cleanDestinationDir
 tar -zcvf 404.tgz public_html
 # as security concern dont write the password here because it would be on git hub than anyone can access it.
 scp 404.tgz jadonbel@jadonbelezos.com:/home/jadonbel/
