59104218997a54bdc52f24193d7f3045def74909
Tom Burris <unsaneity@gmail.com>
2020-07-29 22:03:19 -1000

    tweaked ball bouncing collision behavior

--- content/post/application/pong/js/main.js
+++ content/post/application/pong/js/main.js
@@ -1,78 +1,86 @@
 "use strict";
 const paddleSize = 250;
-const paddleWidth = 20; 
+const paddleWidth = 20;
 const ballSize = 15;
 const distanceFromWall = 30;
-const ballCapSpeed = 15;
+const ballYSpeedIncrease = 1.05;
+const ballCapSpeedX = 15;
+const maxBallCollision = distanceFromWall + paddleWidth + ballSize * 0.5;
+const minBallCollision = distanceFromWall + paddleWidth * 0.5;
+const ballCapSpeedY = (maxBallCollision - minBallCollision) * 0.95;
+let events_p = document.getElementById("events");
+let canvas = document.getElementById("gameBoard");
+let ctx = canvas.getContext("2d");
+const getRandomizedBounce = () => Math.floor(Math.random() * 5) - 2;
 window.gameBoard.ball = {
-		x: 500,
-		y: 200,
-		dx: -4,
-		dy: -4,
+		x: 0,
+		y: 0,
+		dx: 0,
+		dy: 0,
 		reset: function(){
-			let top_or_bottom = (Math.random() > 0.5) ? 1 : -1;
-			this.x = 500;
-			this.y = 200;
-			this.dx = ballCapSpeed*0.5*top_or_bottom;
-			this.dy = ballCapSpeed*0.5*top_or_bottom;
+			const left_or_right = (Math.random() > 0.5) ? 1 : -1;
+			const top_or_bottom = (Math.random() > 0.5) ? 1 : -1;
+			this.x = (Math.random() * 0.8 + 0.1) * canvas.width;
+			this.y = canvas.height / 2;
+			this.dx = ballCapSpeedX*0.5*left_or_right;
+			this.dy = ballCapSpeedY*0.5*top_or_bottom;
 		},
-		
+
 		update: function() {
-		let canvas = document.getElementById("gameBoard");
 			this.x += this.dx;
 			this.y += this.dy;
 
 			//decided to make it a little more intresting!
-			const unPredictableBounce =  Math.floor(Math.random()*4) + -2
+			const unPredictableBounce = getRandomizedBounce();
 			//bounce off the walls
 			if(this.y < 0){
-				goal("top");
-			} 
-			
-			if(this.y  > canvas.height){
 				goal("bottom");
-			} 
-					
-			
-			if(this.x - ballSize > canvas.width){
-				this.x = canvas.width - 1; 
+			}
+
+			if(this.y > canvas.height){
+				goal("top");
+			}
+
+
+			if(this.x + ballSize * 0.75 > canvas.width){
+				this.x = canvas.width - ballSize * 0.75;
 				this.dx = -this.dx + unPredictableBounce;
-				
-			} 
-			if(this.x < 0){
-				this.x =  1; 
-				this.dx = -this.dx+ unPredictableBounce;
-				
-			} 
+
+			}
+			if(this.x < ballSize * 0.75){
+				this.x = ballSize * 0.75;
+				this.dx = -this.dx + unPredictableBounce;
+
+			}
 			// cap dx and dy
-			if (Math.abs(this.dx) >= ballCapSpeed){ 
-				this.dx = (this.dx > 0) ? ballCapSpeed : -ballCapSpeed;
- 			}
-			if (Math.abs(this.dy) >= ballCapSpeed){ 
-				this.dy = (this.dy > 0) ? ballCapSpeed : -ballCapSpeed;
- 			} 
+			if (Math.abs(this.dx) > ballCapSpeedX){
+				this.dx = (this.dx > 0) ? ballCapSpeedX : -ballCapSpeedX;
+			}
+			if (Math.abs(this.dy) > ballCapSpeedY){
+				this.dy = (this.dy > 0) ? ballCapSpeedY : -ballCapSpeedY;
+			}
 		},
-	} 
+	}
 
 
 function setup(){
-	let canvas = document.getElementById("gameBoard");
- 	window.gameBoard.P1X = 20; //top
+	window.gameBoard.ball.reset();
+	window.gameBoard.P1X = 20; //top
 	window.gameBoard.P2X = 20; //bottom
 	window.gameBoard.g2 = 0;
 	window.gameBoard.g1 = 0;
 	window.gameBoard.keys = {
 		top_left: false,
 		top_right: false,
-		bottom_left: false, 
+		bottom_left: false,
 		bottom_right: false,
 	}
-	
-	
+
+
 	drawBoard();
 }
 function goal(who){
-	let goalCount = 0
+	let goalCount = 0;
 	if(who == "top"){
 		window.gameBoard.g1 += 1;
 		goalCount = window.gameBoard.g1;
@@ -81,22 +89,26 @@ function goal(who){
 		goalCount = window.gameBoard.g2;
 	}
 	let GoalCounter = document.getElementById(who);
-	GoalCounter.innerHTML  = goalCount;
-	
-	alert( who + " completed a goal!!");
+	GoalCounter.innerHTML = goalCount;
+
+	clearTimeout(events_p.timeout);
+	events_p.textContent = who + " scored a point!!";
+	events_p.timeout = setTimeout(function() {
+		events_p.textContent = "\u00A0"; // non-breaking space
+	}, 1000);
 	window.gameBoard.ball.reset();
 
 }
 
 
-//set is used so I dont have to write 
+//set is used so I dont have to write
 // the clearkeys which only changes the set!
 function processKeys(e,set){
 	if (e.key == "a"){
 		window.gameBoard.keys.top_left= set;
 	}
 	if (e.key == "d"){
-	
+
 		window.gameBoard.keys.top_right= set;
 	}
 	if (e.keyCode == '37'){ // left arrow
@@ -104,11 +116,11 @@ function processKeys(e,set){
 	}
 	if (e.keyCode == '39'){ // right arrow!
 		window.gameBoard.keys.bottom_right= set;
-		
+
 	}
 }
 
- function updatePaddles(){
+function updatePaddles(){
 	const speedRate = 12;
 	if (window.gameBoard.keys.top_left){
 		window.gameBoard.P1X -= speedRate;
@@ -120,19 +132,18 @@ function processKeys(e,set){
 		window.gameBoard.P2X -= speedRate;
 	}
 	if (window.gameBoard.keys.bottom_right){
-		window.gameBoard.P2X += speedRate;	
+		window.gameBoard.P2X += speedRate;
 	}
-	
-	
+
+
 	// lock the bars from reaching out of the canvas!
-	let canvas = document.getElementById("gameBoard");
 	if (window.gameBoard.P1X + paddleSize > canvas.width){
 		window.gameBoard.P1X = canvas.width - paddleSize;
 	}
 	if (window.gameBoard.P1X < 0){
 		window.gameBoard.P1X = 0;
 	}
-	if (window.gameBoard.P2X  + paddleSize > canvas.width){
+	if (window.gameBoard.P2X + paddleSize > canvas.width){
 		window.gameBoard.P2X = canvas.width - paddleSize;
 	}
 	if (window.gameBoard.P2X < 0){
@@ -142,68 +153,66 @@ function processKeys(e,set){
 
 
 function drawBoard(){
-	let canvas = document.getElementById("gameBoard");
-	let ctx  = canvas.getContext("2d"); 
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	//first part draw the boards 
+	//first part draw the boards
 	ctx.fillRect(window.gameBoard.P1X,distanceFromWall,paddleSize,paddleWidth);
-	ctx.fillRect(window.gameBoard.P2X,canvas.height-distanceFromWall,paddleSize,paddleWidth);
+	ctx.fillRect(window.gameBoard.P2X,canvas.height - distanceFromWall - paddleWidth,paddleSize,paddleWidth);
 
 	// draw the ball!
 	ctx.beginPath();
 	ctx.arc(window.gameBoard.ball.x,window.gameBoard.ball.y,ballSize,0,2 * Math.PI);
 	ctx.fill();
-	ctx.closePath();
-	
+
 	//the line in the middle!
 	ctx.fillRect(0,(canvas.height/2),canvas.width,2);
 }
 
 
- function updateFrame(){
-	 
-	 
+function updateFrame(){
+
+
 	const ball = window.gameBoard.ball;
-	const canvas = document.getElementById("gameBoard");
-	
-	const unPredictableBounce =  Math.floor(Math.random()*4) + -2;
-	window.gameBoard.ball.update();
+
+	const unPredictableBounce = getRandomizedBounce();
+	ball.update();
 	updatePaddles();
-	
-	if(ball.y < distanceFromWall && 
-	  (ball.x < paddleSize + window.gameBoard.P1X) &&
-	  (ball.x > window.gameBoard.P1X)){
-		  // this isnt the best method but until I have decided to write something 
-		  // better it will work for now!!
-		  // we have intercepted the paddle. 
-		  ball.dx = -ball.dx + unPredictableBounce;
-		  ball.dy = -ball.dy;
-		  // bounce the ball!
-	  }
-	  
-	  if(ball.y > canvas.height-distanceFromWall && 
-	  (ball.x < paddleSize + window.gameBoard.P2X) &&
-	  (ball.x > window.gameBoard.P2X)){
-		  // this isnt the best method but until I have decided to write something 
-		  // better it will work for now!!
-		  // we have intercepted the paddle. 
-		  ball.dx = -ball.dx + unPredictableBounce;
-		  ball.dy = -ball.dy;
-		  // bounce the ball!
-	  }
-	  
-	//calling it in update frame, 
-	// makes movement smoother. 
+
+	if(ball.y < maxBallCollision && ball.y > minBallCollision &&
+		(ball.x < paddleSize + window.gameBoard.P1X) &&
+		(ball.x > window.gameBoard.P1X)){
+			ball.y = maxBallCollision;
+			// this isnt the best method but until I have decided to write something
+			// better it will work for now!!
+			// we have intercepted the paddle.
+			ball.dx = ball.dx + unPredictableBounce;
+			ball.dy = -ball.dy * ballYSpeedIncrease;
+			// bounce the ball!
+		}
+
+		if(ball.y > canvas.height-maxBallCollision && ball.y < canvas.height - minBallCollision &&
+		(ball.x < paddleSize + window.gameBoard.P2X) &&
+		(ball.x > window.gameBoard.P2X)){
+			ball.y = canvas.height - maxBallCollision;
+			// this isnt the best method but until I have decided to write something
+			// better it will work for now!!
+			// we have intercepted the paddle.
+			ball.dx = ball.dx + unPredictableBounce;
+			ball.dy = -ball.dy * ballYSpeedIncrease;
+			// bounce the ball!
+		}
+
+	//calling it in update frame,
+	// makes movement smoother.
 	// also can proccess two keys!
-	
+
 	drawBoard();
-	 
- }
- 
+
+}
+
 window.addEventListener("load", function(){
 	setup();
-    setInterval(updateFrame,33); // once every 30 seconds 30 fps!!
+	setInterval(updateFrame,33); // once every 30 seconds 30 fps!!
 	// this for smoother movement!!
 	window.addEventListener("keydown",function(e){processKeys(e,true)});
 	window.addEventListener("keyup",function(e){processKeys(e,false)});
-}); 
+});
