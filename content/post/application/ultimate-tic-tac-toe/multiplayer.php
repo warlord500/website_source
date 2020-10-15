d4639320fcefbb679c1885e8e399def60ebcbfd9
jadon belezos <jjbelezos@gmail.com>
2020-10-14 20:46:16 -0700

    adding php to ultimate tic tac toe

---
+++ content/post/application/ultimate-tic-tac-toe/multiplayer.php
@@ -0,0 +1,14 @@
+/*multiplayer
+create username 
+list active users when create game
+
+keep pool of people 
+then matchmake if more than 2 people in pool
+which means you create a game file. 
+
+soft lock the game board on opponents side.
+
+one person goes. 
+then change board specifical
+
+at end of game reset. */
