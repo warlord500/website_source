e5112f5cdb0d7f9ee73d3dad633c300d72303435
jadon belezos(aka warlord500) <jjbelezos@gmail.com>
2020-10-22 16:13:01 -0700

    i forgot to remove extraneous code

--- content/post/application/ultimate-tic-tac-toe/multiplayer.php
+++ content/post/application/ultimate-tic-tac-toe/multiplayer.php
@@ -13,19 +13,6 @@ one person goes.
 then change board specifical
 
 at end of game reset. */
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
 
 	header("Content-Type: application/json");
 	if (isset($_GET["UserName"])){
