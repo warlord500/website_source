d1891a3ec4bdaf7b3487aa7f40597e7a8c43064b
jadon belezos(aka warlord500) <jjbelezos@gmail.com>
2022-03-23 12:09:39 -0700

    disabling ipfs support for now

--- publishing/publish.sh
+++ publishing/publish.sh
@@ -9,9 +9,9 @@ tar -zcvf 404.tgz public_html
 # as security concern dont write the password here because it would be on git hub than anyone can access it.
 scp 404.tgz jadonbel@jadonbelezos.com:/home/jadonbel/
 ssh jadonbel@jadonbelezos.com "./deploy.sh"
-HASH= $(ipfs.sh add -qr --only-hash /d/website_source/public_html/ | tail -n 1)
-ipfs.sh name publish $HASH
+#HASH= $(ipfs.sh add -qr --only-hash /d/website_source/public_html/ | tail -n 1)
+#ipfs.sh name publish $HASH
 
-echo "publishing to pinata";
-read $LOGINKEY
-ipfs.sh pin remote add $HASH  $LOGINKEY
+#echo "publishing to pinata";
+#read $LOGINKEY
+#ipfs.sh pin remote add $HASH  $LOGINKEY
