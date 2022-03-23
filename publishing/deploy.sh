8950f23ca4ac756a8819c7007b05581b44f24e00
jadon belezos(aka warlord500) <jjbelezos@gmail.com>
2022-03-23 12:20:01 -0700

    turns out it was just deploying wrong location

--- publishing/deploy.sh
+++ publishing/deploy.sh
@@ -5,7 +5,8 @@ rm  -R public_html/*
 #ok my options on this are as follows
 #a solves the fossil reading the cgi script wrong due to incorrect line endings.
 #o overwrites the htaccess that auto spawns on the server
-tar -zxvf  404.tgz
+mv 404.tgz domains/jadonbelezos.com
+tar -zxvf  domains/jadonbelezos.com/404.tgz
 # oddly i dont think this command works.
 # the cgi file must be in unix line endings for fossil to work
 #tr -d "\r" < /home/jadonbel/public_html/cgi-bin/fossil.cgi 
