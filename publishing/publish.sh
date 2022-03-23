f31bb6beaaf3141da03b71e7759efa69cc976866
jadon belezos(aka warlord500) <jjbelezos@gmail.com>
2022-03-23 11:17:06 -0700

    well lets try this again

--- publishing/publish.sh
+++ publishing/publish.sh
@@ -3,7 +3,7 @@ git commit
 git push
 #makes getting the password easier
 /d/portable\ applications/windows/keepass/keepass_classic/KeePass.exe &
-Hugo --cleanDestinationDir --destination "public_html/"
+Hugo --cleanDestinationDir --destination "public_html/" --minify
 rm -R ./public_html/*.ts # cut down on archive space
 tar -zcvf 404.tgz public_html
 # as security concern dont write the password here because it would be on git hub than anyone can access it.
