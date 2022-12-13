15418d533456a819ac0e37c75b333505af0a09f0
jadon belezos(aka warlord500) <jjbelezos@gmail.com>
2022-12-12 23:25:53 -0800

    adding some basic changes to the scrolls of fortune page

--- content/post/application/scrolls_of_fortune/index.md
+++ content/post/application/scrolls_of_fortune/index.md
@@ -14,13 +14,15 @@ custom_css = []
 <!--more-->
 
 
-		scrolls of fortune computer 
+		### scrolls of fortune helper page! 
+
 this is a test algorithim designed to make a reward i like to do in habitica much more interesting 
 and easier i might do some practical effects too!
 
 <button id="decide">decide reward</button>
 <button id="decide_five">five rewards</button>
 <button id="decide_ten">ten rewards</button>
-<p id="scrollsBought"></p>
+<button id="pay_for">pay for the rewards(alter habitica gold)</button>
+<p id="scrollsBought"> scrolls:<span id="numScrolls">0</span> cost: <span id="costScrolls">0gp</span></p>
 <p id="logreward">log: <br/></p>
 <script src="script_for_scroll.js"></script>
--- content/post/application/scrolls_of_fortune/script_for_scroll.js
+++ content/post/application/scrolls_of_fortune/script_for_scroll.js
@@ -1,13 +1,30 @@
-document.getElementById("decide").addEventListener("click",decideReward);
+document.getElementById("decide").addEventListener("click",decide_one);
+document.getElementById("decide_five").addEventListener("click",decide_five);
+document.getElementById("decide_ten").addEventListener("click",decide_ten);
 window.scrollsBought = 0;
-function decideReward(){
+window.scrollsCost = 0;
+function decideReward(scrollsBought,cost){
 	const rewards = ["curse of exercise","curse of traps take d4 quest damage","free crystal set",
 			"gain 2*d10 streak bonus add to any random task","d4 loot bonus + 5 gold","10 day streak bonus"];
 	const selectedReward = Math.floor(Math.random()*rewards.length);
 	//window.alert(rewards[selectedReward]);
-	document.getElementById("logreward").innerHTML += (rewards[selectedReward]) + "<br/>";
-	window.scrollsBought +=1;
-	document.getElementById("scrollsBought").innerHTML = (window.scrollsBought.toString() + " " +
-		(window.scrollsBought*10).toString() + "gp"); 
+	for(let i = 0; i < scrollsBought; i++){
+		let logText = (rewards[selectedReward]) + "<br/>";
+		document.getElementById("logreward").innerHTML += logText;
+	}
+	window.scrollsBought += scrollsBought;
+	window.scrollsCost += cost; 
+	document.getElementById("numScrolls").innerHTML = window.scrollsBought.toString(); 
+	document.getElementById("costScrolls").innerHTML = (window.scrollsCost).toString() + "gp"; 
 
+}
+function decide_one(){
+	decideReward(1,14);
+}
+
+function decide_five() {
+	decideReward(5,60);
+}
+function decide_ten(){
+	decideReward(10,110);
 }
